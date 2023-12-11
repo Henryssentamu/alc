5bac7620efb8eed44f30e7e560bae1171f8ba606
Henryssentamu <ssentamuhenry61@gmail.com>
2023-12-11 18:22:50 +0300

    added a new section on home page

---
+++ main.js
@@ -0,0 +1,31 @@
+
+// function add_margic(){
+//     var scrolosition = window.scrollY || document.documentElement.scrollTop;
+//     var element = document.getElementById("header");
+//     if (scrolosition >= 80){
+//         element.classList.add("smaller")
+//         console.log("it worked")
+//     }
+//     else{
+//         element.classList.remove("smaller")
+//     }
+// }
+
+window.addEventListener('scroll',()=>{
+    var scrolosition = window.scrollY || document.documentElement.scrollTop;
+
+
+    var element = document.getElementById("header");
+    if (scrolosition >= 80){
+        element.classList.add("smaller")
+        console.log("it worked")
+    }
+    else{
+        element.classList.remove("smaller")
+    }
+
+})
+
+// window.onscroll = function(){
+//     add_margic()
+// }
