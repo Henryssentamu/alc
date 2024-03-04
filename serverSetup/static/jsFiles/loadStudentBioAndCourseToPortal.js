628b1f41f6e5d66f4d2c5ac5ee8fd3bb335435a9
Henryssentamu <ssentamuhenry61@gmail.com>
2024-03-04 16:58:30 +0300

    added updates on student dashboard

--- serverSetup/static/jsFiles/loadStudentBioAndCourseToPortal.js
+++ serverSetup/static/jsFiles/loadStudentBioAndCourseToPortal.js
@@ -18,7 +18,8 @@
 // console.log(getStudentBioAndCourseDetails())
 //  console.log(data)
 
-fetch("http://127.0.0.1:5000/getBioDataAndCourseDetails")
+ async  function fetchStudentDetailsData(){
+    return await fetch("http://127.0.0.1:5000/getBioDataAndCourseDetails")
     .then(response => {
         if (! response.ok){
             throw new Error("api in getBioDataAndCourseDetails route did return data")
@@ -26,9 +27,22 @@ fetch("http://127.0.0.1:5000/getBioDataAndCourseDetails")
         return response.json()
     })
     .then(data =>{
-        console.log(data)
+        // console.log(data)
         return data
     })
     .catch(error=>{
         console.log("faced error while fetching student bio data and paid course details:",error)
     })
+}
+
+async function generateHtml(){
+    var  jsonData = await fetchStudentDetailsData()
+    var firstName = jsonData["firstName"]
+    var secondName = jsonData["lastName"]
+    var fullName = firstName + " ." + secondName[0]
+    document.querySelector(".logedInstudentName")
+        .innerHTML = fullName
+    // console.log(firstName)
+}
+
+generateHtml()
